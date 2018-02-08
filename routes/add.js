7a5c10ccd98611da3db3c76b81652e4bf8b591d3
Maya Bello <mbello@ucsd.edu>
2018-02-08 03:03:59 -0800

    On the second to last slide, cant figure it out

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,20 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+exports.addFriend = function(req, res) { 
+    console.log(data);
+
+    res.render('index', {
+
+        "newFriend": [
+          { "name": req.query.name,
+            "image": req.query.description,
+            "imageURL": "http://lorempixel.com/400/400/people"
+          }
+        ]
+      });
+
+data.friends.push(newFriend);
+
+};
+
+
